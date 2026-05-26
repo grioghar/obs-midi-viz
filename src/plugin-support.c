880cbe51a9a7e6ccaf631ee27907b7bbf3e67803
grioghar <thegrio@gmail.com>
2026-05-26 15:47:52 -0500

    fix: drop plugin-support.c, set PIC directly on rtmidi target

--- src/plugin-support.c
+++
@@ -1,4 +0,0 @@
-// Intentionally empty.
-// OBS_DECLARE_MODULE() and OBS_MODULE_USE_DEFAULT_LOCALE() are defined
-// in plugin-main.cpp. Having them here too causes multiple-definition
-// linker errors, so this file is kept but excluded from CMake sources.
